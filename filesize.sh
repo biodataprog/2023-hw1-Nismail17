30e015fd3409de7665a74a52bc8bd848804b63b8
Nora Ismail <nisma004@c04.ib.hpcc.ucr.edu>
2023-10-06 13:54:33 -0700

    This is a homework 1 solution

--- filesize.sh
+++ filesize.sh
@@ -1,2 +1,8 @@
-#!/bin/bash -l
+ #!/bin/bash -l
 # this script should run a command to see how big the file threatened-species.csv.gz is and how big threatened-species.csv is
+
+cp bigdata/gen220/homework/GEN220_data/tabular/threatened-species.csv.gz .
+ 
+echo "hello world"
+
+
